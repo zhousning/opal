63b8f166f41ec6520a6662fa6fe41b39199c3159
zn <244305150@qq.com>
2019-03-02 23:05:49 +0800

    茶庄只显示实名认证的

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,14 +3,14 @@ class TeamsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @childrens = current_user.children
+    @childrens = current_user.children.where(:status => Setting.users.passed)
     @members = @childrens 
     my_invites(@childrens)
   end
 
   def my_invites(users)
     users.each do |u|
-      members = u.children
+      members = u.children.where(:status => Setting.users.passed)
       @members = @members + members.to_a
       my_invites(members)
     end
